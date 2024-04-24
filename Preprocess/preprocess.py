1025c70a833151fe3bb055c9078fd5db80d37eb2
arellanoaxel <axelarellano4@gmail.com>
2024-04-24 19:42:49 -0400

    Added labels

--- Preprocess/preprocess.py
+++ Preprocess/preprocess.py
@@ -37,9 +37,66 @@ def resize_images(input_dir, output_dir, size=(128, 128)):
                 img = img.resize(size, Image.Resampling.LANCZOS)
                 img.save(dest_path)
 
-# Data Paths
-input_path = '../Data/data-subset/train/frames'
-output_path = '../Data/data-subset/train/processed'
+
+def get_labels(input_dir, output_dir):
+    # Ensure the output directory exists
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Dictionary to hold video numbers and their labels
+    labels_dict = {}
+
+    # Loop through all directories in the input directory
+    for video_num in os.listdir(input_dir):
+        video_path = os.path.join(input_dir, video_num)
+        if os.path.isdir(video_path):
+            # Get all text files in this directory
+            txt_files = [f for f in os.listdir(video_path) if f.endswith('.txt')]
+            if txt_files:
+                # Path to the first text file
+                first_file_path = os.path.join(video_path, txt_files[0])
+                # Read the first number from the first file
+                with open(first_file_path, 'r') as file:
+                    first_line = file.readline()
+                    label = int(first_line.split()[0])  # Assuming label is the first number in the line
+                    labels_dict[video_num] = label
+
+    # Convert labels dictionary to an array and save to numpy file
+    labels_array = np.array([labels_dict[key] for key in sorted(labels_dict.keys())])
+    np.save(os.path.join(output_dir, 'labels.npy'), labels_array)
+
+    return labels_dict
+
+# Training Paths
+train_input_path = '../Data/data-subset/train/frames'
+train_output_path = '../Data/data-subset/train/processed'
+
+train_labels = '../Data/data-subset/train/bbox'
+train_labels_out = '../Data/data-subset/train'
+
+
+# Val Paths
+val_input_path = '../Data/data-subset/val/frames'
+val_output_path = '../Data/data-subset/val/processed'
+
+val_labels = '../Data/data-subset/val/bbox'
+val_labels_out = '../Data/data-subset/val'
+
+
+# Testing Paths
+test_input_path = '../Data/data-subset/train/frames'
+test_output_path = '../Data/data-subset/train/processed'
+
+test_labels = '../Data/data-subset/test/bbox'
+test_labels_out = '../Data/data-subset/test'
+
+
 
 # Call the function
-resize_images(input_path, output_path)
+resize_images(train_input_path, train_output_path)
+resize_images(val_input_path, val_output_path)
+resize_images(test_input_path, test_output_path)
+
+get_labels(train_input_path, train_labels_out)
+get_labels(val_input_path, val_labels_out)
+get_labels(test_input_path, test_labels_out)
